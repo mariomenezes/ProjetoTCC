010368c93b0530875ac6eb187bed638d51516c08
mariomenezes <mariomenezescosta@gmail.com>
2016-09-10 23:01:36 -0300

    adicionada leitura MTP, temperatura

--- ControleEquip/ControleEquip.ino
+++ ControleEquip/ControleEquip.ino
@@ -1,12 +1,24 @@
   //----------------------------------------------------------------------------------------------------------//
-
+//Creditos
+/*
+alterado por Fabiano A. Arndt,
+baseados nos créditos abaixo.
+ 
+fabianoallex@gmail.com
+www.youtube.com/fabianoallex
+*/
+//Fim Creditos 
 #include <SPI.h>
 
 //#include <String.h>
 
 #include <Ethernet.h>
 #include <SD.h>
+#include "Dns.h"
 
+//---------------Configuracao sensor temperatura LM35-----
+
+const int LM35 = A0;
 
 //-------------------Configuracao SdCard----------------------------
 
@@ -59,6 +71,15 @@ boolean statusT3 = false; // Variável para o status do led
 boolean statusT4 = false; // Variável para o status do led 
 //----Configuracoes do CLiente NTP----------------------------------------------------------------
 unsigned int localPort = 8888;      // local port to listen for UDP packets
+//host para a primeira tentativa
+const char* host = "ntp02.oal.ul.pt";  // servidor da NTP.br - ver lista acima para todos os servidores da NTP.br
+//const char* host = "192.168.200.254";  // servidor interno 01 - caso tenha um servidor de hora interno, pode ser configurado o nome ou ip na variavel host
+//const char* host = "192.168.200.253";  // servidor interno 02
+ 
+
+DNSClient Dns;
+IPAddress rem_add;
+//
 
 //IPAddress timeServer(132, 163, 4, 101); // time-a.timefreq.bldrdoc.gov NTP server
 // IPAddress timeServer(132, 163, 4, 102); // time-b.timefreq.bldrdoc.gov NTP server
@@ -226,56 +247,115 @@ void loop(){
   }
 }
 //--------------------------------------------------------------------------------------------------------//
+#define LEAP_YEAR(_year) ((_year%4)==0)
+static  byte monthDays[] = {31, 28, 31, 30 , 31, 30, 31, 31, 30, 31, 30, 31};
+ 
+void localTime(unsigned long *timep, byte *psec, byte *pmin, byte *phour, byte *pday, byte *pwday, byte *pmonth, byte *pyear) {
+  unsigned long long epoch =* timep;
+  byte year;
+  byte month, monthLength;
+  unsigned long days;
+   
+  *psec  =  epoch % 60;
+  epoch  /= 60; // now it is minutes
+  *pmin  =  epoch % 60;
+  epoch  /= 60; // now it is hours
+  *phour =  epoch % 24;
+  epoch  /= 24; // now it is days
+  *pwday =  (epoch+4) % 7;
+   
+  year = 70;  
+  days = 0;
+  while((unsigned)(days += (LEAP_YEAR(year) ? 366 : 365)) <= epoch) { year++; }
+  *pyear=year; // *pyear is returned as years from 1900
+   
+  days  -= LEAP_YEAR(year) ? 366 : 365;
+  epoch -= days; // now it is days in this year, starting at 0
+   
+  for (month=0; month<12; month++) {
+    monthLength = ( (month==1) && LEAP_YEAR(year) ) ? 29 : monthDays[month];  // month==1 -> february
+    if (epoch >= monthLength) { epoch -= monthLength; } else { break; }
+  }
+   
+  *pmonth = month;  // jan is month 0
+  *pday   = epoch+1;  // day of month
+}
+
+/**********************************************************************************
+**************************************** FUNÇÕES FORMATAR DATA/HORA ***************
+**********************************************************************************/
+String zero(int a){ if(a>=10) {return (String)a+"";} else { return "0"+(String)a;} }
+ 
+String diaSemana(byte dia){
+  String str[] = {"Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sabado"};
+  return str[dia];
+}
 
 void lerHoraNTP(){
   
-  sendNTPpacket(timeServer); // send an NTP packet to a time server
-
-    // wait to see if a reply is available
-  delay(1000);  
+  
+  //Chamar essa funcao aqui
+    if(Dns.getHostByName(host, rem_add) == 1 ){
+    Serial.println("DNS resolve...");  
+    Serial.print(host);
+    Serial.print(" = ");
+    Serial.println(rem_add);
+    sendNTPpacket(rem_add);
+  } else {
+    Serial.println("DNS fail...");
+    Serial.print("time.nist.gov = ");
+    Serial.println(timeServer); // caso a primeira tentativa não retorne um host válido
+    sendNTPpacket(timeServer);  // send an NTP packet to a time server
+  }
+   
+  delay(1000); //aguarda um segundo, para receber os dados enviados.
+   
   if ( Udp.parsePacket() ) {  
     // We've received a packet, read the data from it
-    Udp.read(packetBuffer,NTP_PACKET_SIZE);  // read the packet into the buffer
-
-    //the timestamp starts at byte 40 of the received packet and is four bytes,
+    Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read the packet into the buffer
+   
+    // the timestamp starts at byte 40 of the received packet and is four bytes,
     // or two words, long. First, esxtract the two words:
-
     unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
     unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);  
+     
     // combine the four bytes (two words) into a long integer
     // this is NTP time (seconds since Jan 1 1900):
     unsigned long secsSince1900 = highWord << 16 | lowWord;  
-    Serial.print("Seconds since Jan 1 1900 = " );
-    Serial.println(secsSince1900);               
-
-    // now convert NTP time into everyday time:
+    Serial.print("Segundos desde 1 de Jan. de 1900 = " );
+    Serial.println(secsSince1900);     
+   
     Serial.print("Unix time = ");
-    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-    const unsigned long seventyYears = 2208988800UL;     
-    // subtract seventy years:
-    unsigned long epoch = secsSince1900 - seventyYears;  
+    const unsigned long seventyYears = 2208988800UL;      // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+    unsigned long epoch = secsSince1900 - seventyYears;  //desconta 70 anos
     // print Unix time:
-    Serial.println(epoch);                               
-
-
-    // print the hour, minute and second:
-    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
-    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
-    Serial.print(':');  
-    if ( ((epoch % 3600) / 60) < 10 ) {
-      // In the first 10 minutes of each hour, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
-    Serial.print(':'); 
-    if ( (epoch % 60) < 10 ) {
-      // In the first 10 seconds of each minute, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.println(epoch %60); // print the second
+    Serial.println(epoch);         
+ 
+    byte ano, mes, dia, dia_semana, hora, minuto, segundo;            
+    localTime(&epoch, &segundo, &minuto, &hora, &dia, &dia_semana, &mes, &ano); //extrai data e hora do unix time
+     
+    Serial.print("Ano: ");
+    Serial.println(ano+1900);
+    Serial.print("Mes: ");
+    Serial.println(mes+1);
+    Serial.print("Dia da semana: ");
+    Serial.println(dia_semana);
+    Serial.print("Dia: ");
+    Serial.println(dia);
+    Serial.print("Hora: ");
+    Serial.println(hora);
+    Serial.print("minunto: ");
+    Serial.println(minuto);
+    Serial.print("segundo: ");
+    Serial.println(segundo);
+     
+    String s = diaSemana(dia_semana) + ", " + zero(dia) + "/" + zero(mes+1) + "/" + (ano+1900) + " " + zero(hora) + ":" + zero(minuto) + ":" + zero(segundo);
+     
+    Serial.println(s);
+    Serial.println(" ");
   }
-  // wait ten seconds before asking for the time again
-  delay(10000);  
+   
+  delay(10000); //atualiza novamente em 10 segundos
 }
 
 //---------------------------------------------------------------------
@@ -302,6 +382,14 @@ unsigned long sendNTPpacket(IPAddress& address)
   Udp.beginPacket(address, 123); //NTP requests are to port 123
   Udp.write(packetBuffer,NTP_PACKET_SIZE);
   Udp.endPacket(); 
+}
+
+int lerTemperatura()
+{
+  int temperatura = ((int)analogRead(LM35)*5/(1023))/0.01;
+  Serial.print("Temperatura: ");
+  Serial.println(temperatura);
+  return temperatura; 
 }
 
 void escreveArffSd(){
