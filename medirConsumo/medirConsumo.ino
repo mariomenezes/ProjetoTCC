3fd766da182496b814dfc4939ac86ab6ec3e566e
mariomenezes <mariomenezescosta@gmail.com>
2016-11-02 17:28:47 -0300

    correcoes

--- medirConsumo/medirConsumo.ino
+++ medirConsumo/medirConsumo.ino
@@ -96,36 +96,14 @@ void setup(void)
   Serial.println("");
   pinMode(tomada1, OUTPUT);
   digitalWrite(tomada1, HIGH);
-  
-  Ethernet.begin(mac, ip);
-  //Serial.print("IP Arduino ");
-  //Serial.println(Ethernet.localIP());
-  Udp.begin(localPort);
-  //Serial.println("Esperando Sincronizar NTP");
-  setSyncProvider(getNtpTime);
- 
+   
 }
 
-time_t prevDisplay = 0; // when the digital clock was displayed
 
 void loop(void)
 {
   calculaS0();
   
-    if (timeStatus() != timeNotSet) {
-    if (now() != prevDisplay) { //update the display only if time has changed
-      prevDisplay = now();
-      //digitalClockDisplay();  
-    }
-  }
-  
-  //Chama a rotina de desenho na tela
- /* u8g.firstPage();
-  do
-  {
-    draw();
-  }
-  while ( u8g.nextPage() );*/
   delay(150);
 }
 
@@ -285,94 +263,5 @@ float calculaS3(){
   Serial.print("Consp KWH S3: ");
   Serial.println(energy);
   return energy;
-}
-
-void gravarSD(){
-  
-  char buff[20]; 
-  float c0 = calculaS0();
-  float c1 = calculaS1();
-  float c2 = calculaS2();
-  float c3 = calculaS3();
-  String consumo = "Consumo" + hour() + minute() + month() + year();
-  
-  // + "," + dtostrf(c0, 0, 2, buff) + "," + dtostrf(c1, 0, 2, buff) + "," + dtostrf(c2, 0, 2, buff) + "," + dtostrf(c3, 0, 2, buff);
-  
-  if (!SD.begin(4)) {
-    Serial.println("initialization failed!");
-    return;
-  }
-  Serial.println("initialization done.");
-  
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  myFile = SD.open("test.txt", FILE_WRITE);
-  
-  // if the file opened okay, write to it:
-  if (myFile) {
-    Serial.print("Escrevendo consumo.txt:");
-    Serial.println(consumo + dtostrf(c0, 0, 2, buff) + dtostrf(c1, 0, 2, buff));
-    myFile.println(consumo + dtostrf(c0, 0, 2, buff) + dtostrf(c1, 0, 2, buff));
-	// close the file:
-    myFile.close();
-    Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
- 
-}
-
-/*-------- NTP code ----------*/
-
-const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
-byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
-
-time_t getNtpTime()
-{
-  //delay(20000);
-  while (Udp.parsePacket() > 0) ; // discard any previously received packets
-  Serial.println("Transmit NTP Request");
-  sendNTPpacket(timeServer);
-  uint32_t beginWait = millis();
-  while (millis() - beginWait < 1500) {
-    int size = Udp.parsePacket();
-    if (size >= NTP_PACKET_SIZE) {
-      Serial.println("Receive NTP Response");
-      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
-      unsigned long secsSince1900;
-      // convert four bytes starting at location 40 to a long integer
-      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
-      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
-      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
-      secsSince1900 |= (unsigned long)packetBuffer[43];
-      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
-    }
-  }
-  Serial.println("No NTP Response :-(");
-  return 0; // return 0 if unable to get the time
-}
-
-// send an NTP request to the time server at the given address
-void sendNTPpacket(IPAddress &address)
-{
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:                 
-  Udp.beginPacket(address, 123); //NTP requests are to port 123
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
-  Udp.endPacket();
 }
 
